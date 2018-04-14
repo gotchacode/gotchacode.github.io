547aa72eebb63b39418521fb9b0c4d8d92d133c3
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2018-04-14 22:55:30 +0530

    update the css and design on basis of latest white paper theme

--- gruntfile.js
+++ gruntfile.js
@@ -7,21 +7,33 @@ module.exports = function(grunt) {
         src: [
           'css/base.css',
           'css/syntax/emacs.css',
-          'css/octicons.css'
+          'css/octicons.css',
+          'css/font-awesome.min.css'
         ],
         dest: 'css/<%= pkg.name %>.add.css'
       }
     },
-    
+
     cssmin: {
       'css/main.min.css': [
         '<%= concat.dist.dest %>'
+      ],
+      'css/main.min.rtl.css': [
+        '<%= rtlcss.rtl.dest %>'
       ]
+    },
+
+    rtlcss: {
+      rtl: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'css/<%= pkg.name %>.add.rtl.css'
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.registerTask('default', ['concat', 'cssmin']);
-  grunt.registerTask('css', ['concat', 'cssmin']);
+  grunt.loadNpmTasks('grunt-rtlcss');
+  grunt.registerTask('default', ['concat', 'rtlcss', 'cssmin']);
+  grunt.registerTask('css', ['concat', 'rtlcss', 'cssmin']);
 };
